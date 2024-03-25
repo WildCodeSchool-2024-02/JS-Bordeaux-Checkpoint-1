8ef999746a4adcfe7c76509d388331ccd5073895
PAUL BOURY <paulboury17@gmail.com>
2024-03-25 12:10:56 +0100

    Step3 JS succes

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,9 +4,14 @@ imgAvatar.addEventListener('click', function (){
     imgAvatar.alt = "Avatar bis";
 });
 
+const sectionPinkBG = document.querySelector('.pink-bg')
+const button = document.querySelector('.button')
 const firstName = document.querySelector('#firstname');
-firstName.addEventListener('click', function (){
+button.addEventListener('click', function (){
     let changedName = prompt("What's your name?");
     firstName.innerHTML = firstName.innerHTML.replace('Matt', `${changedName}`);
     changedName = firstName.style.color = 'white'; 
+    let changedBackground = prompt("What color do you want?");
+    sectionPinkBG = sectionPinkBG.style.background = changedBackground;
+
 });
