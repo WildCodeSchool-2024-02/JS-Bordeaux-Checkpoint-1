abb05db6a2d5177f60ba05c84ce7786b9584fdf1
Franck <contact@franckwebpro.com>
2024-03-25 12:20:23 +0100

    algo 4

---
+++ algo/4/theaterSieges.js
@@ -0,0 +1,36 @@
+/*
+Un employé de théâtre souhaite obtenir la liste de tous les sièges de sa salle principale.
+Dans la salle les places sont réparties comme suit :
+   - Il y a 26 colonnes de sièges, numérotées de "1" » à "26".
+   - Chaque colonne contient 100 sièges, numérotés de "1" à "100".
+
+Complète la fonction theaterSieges pour qu'elle renvoie un tableau
+où chaque sous-tableau répertorie les positions des sièges dans une rangée.
+
+exemple du résultat final :
+
+[
+  ["1-1", "1-2", "1-3", ..., "1-100"],
+  ["2-1", "2-2", "2-3", ..., "2-100"],
+  ...
+  ["26-1", "26-2", "26-3", ..., "26-100"]
+]
+*/
+
+function theaterSieges() {
+  const allSeat = [];
+  const numberOfColumn = 26;
+  let columnOfSeat = [];
+  for(let i = 1; i <= numberOfColumn; i++) {
+    columnOfSeat =[];
+    for(let y = 1; y <= 100; y++) {
+      columnOfSeat.push(`${i}-${y}`);
+    }
+    allSeat.push(columnOfSeat);
+  }
+  return allSeat;
+}
+
+theaterSieges();
+
+module.exports = theaterSieges;
