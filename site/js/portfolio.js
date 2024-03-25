38a00cce057cd720d96182662104d0151e54e938
AXEL JANIAK <axeljpro@gmail.com>
2024-03-25 12:41:23 +0100

    finish step 2 and begun step 3

---
+++ site/js/portfolio.js
@@ -0,0 +1,23 @@
+const imgAvatar = document.getElementById('imgAvatar');
+
+const changeAvatar = () => {
+    imgAvatar.addEventListener('click', () => {
+        if (imgAvatar.src === "site/image/avatar-bis.png") {
+            imgAvatar.src = "image/avatar.svg";
+        } else {
+            imgAvatar.src = "image/avatar.svg";
+        }
+    })
+}
+
+changeAvatar();
+
+const buttonText = document.getElementById('buttonText');
+const firstName = document.getElementById('firstname');
+const bgColor = document.getElementsByClassName('pink-bg');
+
+buttonText.addEventListener('click', () => {
+    firstName.innerHTML = prompt('enter your text');
+    firstName.style.color = '#FFFFFF';
+    bgColor.style.backgroundColor = prompt('Color?');
+})
