924fe198bb969ebc4bf37ca2b9212325c9b06f64
Jade Jolivet <jade.jolivet@gmail.com>
2024-03-25 13:09:24 +0100

    step 2 js finish

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,3 +6,18 @@ avatarImg.onclick = function() {
   avatarImg.style.width = "150px";
   avatarImg.style.height = "150x";
 };
+
+let button = document.createElement("button");
+button.textContent = "Enter your name and change text color";
+button.onclick = changeName;
+
+document.querySelector("article").appendChild(button);
+
+function changeName(){
+    let newName = prompt ("Enter your name : ");
+
+    let firstName = document.getElementById("firstname");
+    firstName.textContent = newName;
+    firstName.style.color = "white";
+
+}
