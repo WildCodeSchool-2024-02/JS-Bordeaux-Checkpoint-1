957f7651694ba22203ee6801ee0ac4f778d8c78e
PAUL BOURY <paulboury17@gmail.com>
2024-03-25 11:52:06 +0100

    Step2 JS succes

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,10 @@ imgAvatar.addEventListener('click', function (){
     imgAvatar.src = "./image/avatar-bis.png";
     imgAvatar.alt = "Avatar bis";
 });
+
+const firstName = document.querySelector('#firstname');
+firstName.addEventListener('click', function (){
+    let changedName = prompt("What's your name?");
+    firstName.innerHTML = firstName.innerHTML.replace('Matt', `${changedName}`);
+    changedName = firstName.style.color = 'white'; 
+});
