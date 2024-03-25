fade74218fc43361617e8d53b1e03b0cdeae5b86
JulienChevreauPro <julienchevreau33@gmail.com>
2024-03-25 12:22:52 +0100

    finished step 1 js

---
+++ site/js/portfolio.js
@@ -0,0 +1,6 @@
+let imgAvatar= document.querySelector (".avatar");
+imgAvatar.addEventListener ("click", function() {
+    if (imgAvatar.src= "./image/avatar.svg") {
+        imgAvatar.src= "./image/avatar-bis.png";
+    }
+});
