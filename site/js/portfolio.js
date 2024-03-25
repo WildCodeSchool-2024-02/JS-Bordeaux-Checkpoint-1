c53dd20741c26fc0932d80ce8f77972c3ce1834a
Franck <contact@franckwebpro.com>
2024-03-25 10:48:01 +0100

    second step JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,13 +1,27 @@
- const bannerImg = document.getElementById('bannerImg');
+const bannerImg = document.getElementById('bannerImg');
 
- const changeBannerImg = () => {
+const changeBannerImg = () => {
     bannerImg.addEventListener('click', () => {
-        if(bannerImg.src === "image/avatar-bis.png") {
-        bannerImg.src = "image/avatar.svg";
+        if (bannerImg.src === "image/avatar-bis.png") {
+            bannerImg.src = "image/avatar.svg";
         } else {
-        bannerImg.src = "image/avatar-bis.png";
+            bannerImg.src = "image/avatar-bis.png";
         }
     });
- }
+}
 
 changeBannerImg();
+
+const modifyName = document.getElementById('modifyName');
+const firstName = document.getElementById('firstname');
+const whiteColor = "#ffffff";
+
+const changeNameAndColor = () => {
+    modifyName.addEventListener('click', () => {
+        const newName = prompt('Enter your name');
+        firstName.innerText = newName;
+        firstName.style.color = whiteColor;
+    });
+}
+
+changeNameAndColor();
