baf247993cd53d71f02a8459b3ea78e9e75feb76
PAUL BOURY <paulboury17@gmail.com>
2024-03-25 11:28:16 +0100

    Step1 JS succes

---
+++ site/js/portfolio.js
@@ -0,0 +1,5 @@
+const imgAvatar = document.querySelector('.avatar');
+imgAvatar.addEventListener('click', function (){
+    imgAvatar.src = "./image/avatar-bis.png";
+    imgAvatar.alt = "Avatar bis";
+});
