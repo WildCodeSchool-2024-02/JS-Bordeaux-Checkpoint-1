6aeb4842a96dc0ce74a8859a2541349556f7ee0d
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-03-25 13:06:28 +0100

    finish step 6 JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -34,3 +34,21 @@ const changeColorLink = document.querySelectorAll('a');
     changeColorLink.forEach(element => {
         element.style.color = '#750ff7';
     });
+
+const changeAllLi = document.getElementById('modify');
+const modify = document.createElement('button');
+modify.textContent = 'Modify';
+modify.style.backgroundColor = '#750ff7';
+modify.style.color = 'white';
+modify.style.padding = '0.5rem';
+modify.style.borderRadius = '0.5rem';
+changeAllLi.appendChild(modify);
+
+modify.addEventListener('click', function() {
+    newText = ['VSCode','Github','Terminal'];
+
+    const modifyLi = document.querySelectorAll('.devoLi');
+    modifyLi.forEach(function(element, index) {
+        element.textContent = newText[index]
+    })
+})
