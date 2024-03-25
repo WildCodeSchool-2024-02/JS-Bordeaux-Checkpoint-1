2256db1696fc515f4d66c8162bb91fb4ae84eebd
Jade Jolivet <jade.jolivet@gmail.com>
2024-03-25 12:40:19 +0100

    avatar js finish

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+let avatarImg = document.querySelector(".avatar1");
+
+avatarImg.onclick = function() {
+
+  avatarImg.src = "image/avatarGirl.png";
+  avatarImg.style.width = "150px";
+  avatarImg.style.height = "150x";
+};
