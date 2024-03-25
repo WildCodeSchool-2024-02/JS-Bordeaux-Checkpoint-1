e05a8efc9aee80959ea3e08f4f76407837bde879
FGAT3 <feliciengamelin@yahoo.fr>
2024-03-25 12:38:55 +0100

    JS step 3 & 4 finished

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,12 +1,27 @@
 const getAvatar = document.getElementById('avatar');
-const getName = document.getElementById('firstname');
 
 getAvatar.addEventListener('click', function () {
     getAvatar.src="image/avatar-bis.png";
-})
+});
 
-getName.addEventListener('click', function () {
-    const newName = prompt("Enter your name");
-    getName.innerHTML = newName;
-    getName.style.color = "white";
-})
+const namePortfolio = document.getElementById('firstname');
+const description = document.getElementById('description');
+const thirdTitle = document.querySelectorAll('h4');
+const footer = document.querySelector('footer');
+
+namePortfolio.addEventListener('click', function () {
+    const newColor = prompt('Enter a color');
+    const newName = prompt('Enter your name');
+    namePortfolio.innerHTML = newName;
+    namePortfolio.style.color = 'white';
+    if (newColor === '#750ff7') {
+        description.classList.remove('pink-bg');
+        description.classList.add('purple-bg');
+        thirdTitle.forEach(title => {
+            title.classList.remove('pink-text');
+            title.classList.add('purple-text');
+        })
+        footer.classList.remove('pink-bg');
+        footer.classList.add('purple-bg');
+    };
+});
