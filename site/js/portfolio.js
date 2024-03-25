75d8fac29a6da71f31cb7d79d8e2fd4a7867a420
JulienChevreauPro <julienchevreau33@gmail.com>
2024-03-25 12:50:06 +0100

    step 2 js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,16 @@ imgAvatar.addEventListener ("click", function() {
         imgAvatar.src= "./image/avatar-bis.png";
     }
 });
+
+
+let firstName= document.getElementById("firstname");
+firstName.addEventListener ("click", () =>{
+    firstName.innerText= prompt("enter your name");
+    firstName.style.color= "white";
+});
+// (tried to create a button)
+// let description= document.querySelector (".description pink-bg");
+// let button= document.createElement ("button");
+// button.innerText= "modify text and color";
+// button.style.marginBottom= "2rem";
+// description.appendChild(button);
